cc3d97b41e32d0cd26f005251df97d265564a648
Harold <kaendor50@gmail.com>
2026-07-13 23:02:08 -0600

    Update Firebase Config with real credentials

--- tasklyzen-auth.js
+++ tasklyzen-auth.js
@@ -8,12 +8,13 @@
 (function exposeTasklyzenAuth(global) {
     // IMPORTANTE: Reemplaza estos valores con la configuración real de tu proyecto en Firebase
     const firebaseConfig = {
-        apiKey: "REPLACE_WITH_YOUR_API_KEY",
-        authDomain: "REPLACE_WITH_YOUR_PROJECT_ID.firebaseapp.com",
-        projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-        storageBucket: "REPLACE_WITH_YOUR_PROJECT_ID.appspot.com",
-        messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-        appId: "REPLACE_WITH_YOUR_APP_ID"
+        apiKey: "AIzaSyC5NF5Z_lxbhTfKyHnSzfaqvEKsFG489OI",
+        authDomain: "tasklyzen-app.firebaseapp.com",
+        projectId: "tasklyzen-app",
+        storageBucket: "tasklyzen-app.firebasestorage.app",
+        messagingSenderId: "918293539965",
+        appId: "1:918293539965:web:ff13d058cb5c9f9bf55514",
+        measurementId: "G-W5CHSH82BK"
     };
 
     let auth = null;
